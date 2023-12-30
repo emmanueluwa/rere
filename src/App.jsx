496e267f09e368678d8c9d676168b72cc65caa84
emmanueluwa <emmanuel@fulodev.com>
2023-12-30 12:04:41 +0000

    adding ability to get data from array for influencer card

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,46 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
 import Influencer from "./components/Influencer";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("pet food");
 
   const [influencers, setInfluencers] = useState([
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "jon",
+      role: "events",
+      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
     },
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "talo",
+      role: "designer",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
     },
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "safiya",
+      role: "engineer",
+      img: "https://images.pexels.com/photos/11606423/pexels-photo-11606423.jpeg",
     },
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "maryam",
+      role: "outreach",
+      img: "https://images.pexels.com/photos/15046702/pexels-photo-15046702/free-photo-of-portrait-of-a-woman-dressed-in-black.jpeg",
     },
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "mo",
+      role: "accountant",
+      img: "https://images.pexels.com/photos/18935840/pexels-photo-18935840/free-photo-of-portrait-of-a-man-wearing-black-shirt.jpeg",
     },
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "tim",
+      role: "construction",
+      img: "https://images.pexels.com/photos/12190564/pexels-photo-12190564.jpeg",
     },
     {
-      name: "fulo",
-      role: "builder",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+      name: "aisha",
+      role: "qa engineer",
+      img: "https://images.pexels.com/photos/9380925/pexels-photo-9380925.jpeg",
     },
   ]);
 
@@ -67,58 +68,16 @@ function App() {
             }}
           />
           <div className="flex flex-wrap justify-center">
-            <Influencer
-              name="fulo"
-              role="builder"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="usman"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="esa"
-              role={role}
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="fulo"
-              role="builder"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="usman"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="esa"
-              role={role}
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="fulo"
-              role="builder"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="usman"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer name="esa" role={role} />
-            <Influencer
-              name="fulo"
-              role="builder"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="usman"
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
-            <Influencer
-              name="esa"
-              role={role}
-              img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-            />
+            {influencers.map((influencer) => {
+              return (
+                <Influencer
+                  key={uuidv4()}
+                  name={influencer.name}
+                  role={influencer.role}
+                  img={influencer.img}
+                />
+              );
+            })}
           </div>
         </div>
       ) : (
