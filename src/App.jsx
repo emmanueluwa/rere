0d55798f945135ded56ae307bccd67cd8b8a79f9
emmanueluwa <emmanuel@fulodev.com>
2023-12-31 20:43:26 +0000

    adding detail page for customers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import InfluencersPage from "./pages/InfluencersPage";
 import CustomersPage from "./pages/CustomersPage";
+import CustomerPage from "./pages/CustomerPage";
 
 import DictionaryPage from "./pages/DictionaryPage";
 import DefinitionPage from "./pages/DefinitionPage";
@@ -16,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/influencers" element={<InfluencersPage />} />
           <Route path="/customers" element={<CustomersPage />} />
+          <Route path="/customers/:id" element={<CustomerPage />} />
+
           <Route path="/dictionary" element={<DictionaryPage />} />
           <Route path="/dictionary/:search" element={<DefinitionPage />} />
           <Route path="/404" element={<NotFoundPage />} />
---
+++ src/pages/CustomerPage.jsx
@@ -0,0 +1,32 @@
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function CustomerPage() {
+  const { id } = useParams();
+  const [customer, setCustomer] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:8000/api/customers/" + id;
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCustomer(data.customer);
+      });
+  }, []);
+
+  return (
+    <>
+      {customer ? (
+        <div>
+          <p>{customer.id}</p>
+          <p>{customer.name}</p>
+          <p>{customer.industry}</p>
+        </div>
+      ) : null}
+      <Link to="/customers">Go back</Link>
+    </>
+  );
+}
